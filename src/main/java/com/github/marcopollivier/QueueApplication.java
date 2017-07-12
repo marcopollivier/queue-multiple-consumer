88e2efe30b9a29bd6d8ec180599c0233cc3a7eca
Marco Paulo Ollivier <marcopollivier@gmail.com>
2017-07-12 02:50:24 -0300

    Limpando código

--- src/main/java/com/github/marcopollivier/QueueApplication.java
+++ src/main/java/com/github/marcopollivier/QueueApplication.java
@@ -2,24 +2,11 @@ package com.github.marcopollivier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-import java.time.Clock;
-
-@EntityScan(
-        basePackageClasses = { QueueApplication.class, Jsr310JpaConverters.class }
-)
 @SpringBootApplication
 @Configuration
 public class QueueApplication {
-
-    @Bean
-    public Clock clock() {
-        return Clock.systemDefaultZone();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(QueueApplication.class, args);
