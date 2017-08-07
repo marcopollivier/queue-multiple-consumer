29bba8346d7e898f7b610d58af7b70ff64c714ff
Marco Paulo Ollivier <marcopollivier@gmail.com>
2017-08-07 16:50:12 -0300

    Separa as responsabilidades em projetos
    - producer-magneto
    - consumer-cypher
    - consumer-juggernaut

---
+++ consumer-juggernaut/src/main/java/com/github/marcopollivier/adapter/messaging/QueueConstants.java
@@ -0,0 +1,18 @@
+package com.github.marcopollivier.adapter.messaging;
+
+/**
+ * Created by marcoollivier on 05/06/17.
+ */
+final class QueueConstants {
+
+    static final String FATURA_CONVERTIDA_OUTPUT = "FilaUnicaOutput";
+    static final String FATURA_CONVERTIDA_CYPHER_INPUT = "FilaUnicaInputCypher";
+    static final String FATURA_CONVERTIDA_JUGGERNAUT_INPUT = "FilaUnicaInputJuggernaut";
+
+    /**
+     * private constructor
+     */
+    private QueueConstants() {// prevent instantiation
+    }
+
+}
