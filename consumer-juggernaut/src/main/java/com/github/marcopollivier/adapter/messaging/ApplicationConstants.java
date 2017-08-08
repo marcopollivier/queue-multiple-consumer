34e5f18e1db6d086a91642db8a64fffe4181e7e7
Marco Paulo Ollivier <marcopollivier@gmail.com>
2017-08-08 11:51:07 -0300

    Remove consumidor extra
    Não tem necessidade de manter código duplicado para esse cenário. Se um consumidor não pegar as mensagens que não são dele, pode ser considerado como correto

--- consumer-juggernaut/src/main/java/com/github/marcopollivier/adapter/messaging/ApplicationConstants.java
+++
@@ -1,9 +0,0 @@
-package com.github.marcopollivier.adapter.messaging;
-
-public class ApplicationConstants {
-
-    public static final String TENANT = "TENANT";
-    public static final String CYPHER = "cypher";
-    public static final String JUGGERNAUT = "juggernaut";
-
-}
